b003f5b643aff46916ea323a10baa5483599c136
Marques103-23187257 <aluno103.23187257gabriel@gmail.com>
2024-03-07 21:37:39 -0300

    ajustando os metodos de gerenciamento de consultas e adicionando um metodo de impressão

--- OOP/DesafioClinica.py
+++ OOP/DesafioClinica.py
@@ -14,28 +14,37 @@ class Funcionario:
             }
         lista.append(consulta)
         print('Consulta agendada!')
+
     
     def GerenciarConsulta(self, lista, cpf, novo_horario, nova_data, cancelar=False):
 
+        
         if len(lista) == 0:
             print('Não há consultas marcadas na lista')
+        
         else:
             for consulta in lista:
-                if consulta['CPF'] != cpf:
-                    continue
-                else:
+                if consulta['CPF'] == cpf:
                     if cancelar == True:
                         lista.remove(consulta)
                         print('Consulta Cancelada')
-                    else:
-                        pass
 
-
-
-
-
+    def ImprimirConsultas(self, lista):
+        from datetime import datetime
+        print(f"{10*'-'}Consultas{11*'-'}\n\nRelatorio gerado por: {self.nome}\nHorario: {datetime.today()}\n{30*'_'}")
         
-    
+        for consulta in lista:
+            paciente, cpf, medico, data, horario, responsavel = consulta.values()
+            print(f'''
+Paciente: {paciente}
+CPF: {cpf}
+Medico: {medico}
+Data: {data}
+Horario: {horario}
+Marcado por: {responsavel}
+{30*'_'}
+''')
+        
     def FazerExame(self, exame):
         print(f'Realizando exame: {exame}')
     
@@ -45,7 +54,7 @@ class Medico(Funcionario):
         self.crm = crm
     
     def PrescMedicamento(self, medicamento):
-        print(f'Prescrição medica: {medicamento}' )
+        print(f'Prescrição medica: {medicamento}')
 
 class Enfermeira(Funcionario):
     def __init__(self, nome, especialidade, coren):
@@ -58,16 +67,11 @@ class Enfermeira(Funcionario):
         
 lista_consultas = []
 
+medico_1 = Medico(nome='Gabriel', especialidade='Medico Psiquiatra', crm='xxxxxxxxxxx')
     
 funcionario_1 = Funcionario(nome='Antony', especialidade='Recepcionista')
-funcionario_1.AgendarConculta()
+funcionario_1.AgendarConculta(paciente='enzo', cpf='01', medico=medico_1.nome, data='22/04/2024', horario='09:00', lista=lista_consultas)
 
-funcionario_2 = Funcionario(nome='Gleisy', especialidade='auxiliar')
+funcionario_1.ImprimirConsultas(lista_consultas)
 
-medico_1 = Medico(nome='Gabriel', especialidade='Medico Psiquiatra', crm='xxxxxxxxxxx')
-
-
-
-
-
-    
+# funcionario_2 = Funcionario(nome='Gleisy', especialidade='auxiliar')
